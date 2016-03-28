445af180281fa1a34e2366dcabbe2ea35b4847d0
onlykey <github@crp.to>
2016-03-28 13:56:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
-      "Only.html", {
+      "OnlyKey.html", {
         innerBounds: { width: 1060, height: 840, minWidth: 1060 }
       });
 });
