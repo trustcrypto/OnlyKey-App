591dbb4cc5c3a8eecf6746369aff4cce2fba493e
The Other Guy <r@crp.to>
2016-09-08 13:37:57 -0400

    ok photo background and other small style tweaks

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var onlyKey = new OnlyKey();
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
       "OnlyKey.html", {
-        innerBounds: { width: 1060, height: 840, minWidth: 1060 }
+        outerBounds: { width: 1024, height: 768 }
       });
 });
 
