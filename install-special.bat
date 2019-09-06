fa16fae9aa990d21ddfb15e12b9766029e441a2b
rodgolpe <r@crp.to>
2019-09-05 22:59:57 -0400

    special installers

---
+++ install-special.bat
@@ -0,0 +1,8 @@
+set npm_config_target=0.38.4
+set npm_config_arch=x64
+set npm_config_target_arch=x64
+set npm_config_runtime=node-webkit
+set npm_config_build_from_source=true
+set npm_config_node_gyp=C:\Users\Rodney\AppData\Roaming\npm\node_modules\nw-gyp\bin\nw-gyp.js
+npm install --msvs_version=2015
+npm i
