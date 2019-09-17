b9fb596b5f6f7c90c75fdd4887f1c6f47368fb21
rodgolpe <r@crp.to>
2019-09-16 23:29:41 -0400

    updated slots tab with new content and styles

--- app/scripts/onlyKey/OnlyKeyWizard.js
+++ app/scripts/onlyKey/OnlyKeyWizard.js
@@ -876,31 +876,6 @@ if (chrome.passwordsPrivate) {
   }, false);
 })();
 
-
-class DialogMgr {
-  open(el, keepOthersOpen) {
-    if (!keepOthersOpen) {
-      this.closeAll();
-    }
-    if (!el.open) {
-      el.showModal();
-    }
-  }
-
-  close(el) {
-    if (el.open) {
-      el.close();
-    }
-  }
-
-  closeAll() {
-    const allDialogs = document.getElementsByTagName('dialog');
-    for (let i = 0; i < allDialogs.length; i++) {
-      this.close(allDialogs[i]);
-    }
-  }
-}
-
 function clearRadios(name) {
   var btns = document.getElementsByName(name);
   for (var i = 0; i < btns.length; i++) {
