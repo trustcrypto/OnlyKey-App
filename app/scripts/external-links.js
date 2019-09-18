87145ba47813ea69ad7860ea47d664d071f12abe
rodgolpe <r@crp.to>
2019-09-18 01:04:10 -0400

    fix external links

--- app/scripts/external-links.js
+++ app/scripts/external-links.js
@@ -1,8 +1,11 @@
 if (typeof nw !== 'undefined') {
     document.querySelector('#main').addEventListener('click', evt => {
-        if ((evt.target !== evt.currentTarget) && (!!evt.target.href && evt.target.className.includes('external'))) {
+        const parent = (evt.path && evt.path[1]) || {};
+        const href = evt.target && evt.target.href ? evt.target.href : parent.href;
+
+        if ((evt.target !== evt.currentTarget) && (!!href && (evt.target.classList.contains('external') || parent.classList.contains('external')))) {
             // nw.Window.open(evt.target.href);
-            nw.Shell.openExternal(evt.target.href);
+            nw.Shell.openExternal(href);
             evt.preventDefault();
             evt.stopPropagation();
         }
