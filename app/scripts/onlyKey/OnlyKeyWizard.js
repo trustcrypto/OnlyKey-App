387b370753e884da5f79434598505adb4d2fcd14
onlykey <onlykey@users.noreply.github.com>
2020-07-17 14:17:20 -0400

    Revert "eddsa fix"
    This reverts commit 0725a8dd627ddb22a40d15bbb25846e7dac9600e.

--- app/scripts/onlyKey/OnlyKeyWizard.js
+++ app/scripts/onlyKey/OnlyKeyWizard.js
@@ -414,7 +414,7 @@ if (chrome.passwordsPrivate) {
 
       const keys = [{
         name: 'Primary Key',
-        s: rawKey.primaryKey.params[2].data
+        s: rawKey.primaryKey.params[1].data.slice(1,rawKey.primaryKey.params[1].length)
       }];
 
       rawKey.subKeys.forEach((subKey, i) => {
