feeb58ca9ff3e3c7f48026bfc43519d383fccfea
Rodney G <rodney@golpenet.com>
2021-08-16 23:26:22 -0400

    upgrade to nw 0.55.0 and use tray (#173)
    * upgrade nw to 0.55.0
    * use tray instead of menubar
    * remove all SVG files from images/brands
    * remove and ignore package-lock because Win and Linux don't like appDmg

---
+++ app/scripts/tray.js
@@ -0,0 +1,60 @@
+(function() {
+  'use strict';
+
+  if (typeof nw == 'undefined') return;
+
+  const userPreferences = require('./scripts/userPreferences.js');
+  const settingsMenu = new nw.Menu();
+
+  settingsMenu.append(new nw.MenuItem({
+      label: 'Auto-launch app on system login',
+      click: function() {
+          userPreferences.autoLaunch = !userPreferences.autoLaunch;
+          console.info(`Toggled autoLaunch to ${userPreferences.autoLaunch}`);
+
+          const AutoLaunch = require('auto-launch');
+          const autoLaunch = new AutoLaunch({
+              name: 'OnlyKey',
+              isHidden: true,
+          });
+
+          autoLaunch.isEnabled()
+              .then(isEnabled => {
+                  if (isEnabled && !userPreferences.autoLaunch) {
+                      autoLaunch.disable();
+                  } else if(!isEnabled && userPreferences.autoLaunch) {
+                      autoLaunch.enable();
+                  }
+              })
+              .catch(console.error);
+      },
+      type: 'checkbox',
+      checked: userPreferences.autoLaunch,
+  }));
+
+  settingsMenu.append(new nw.MenuItem({
+      label: 'Automatically check for app updates',
+      click: function() {
+          userPreferences.autoUpdate = !userPreferences.autoUpdate;
+          console.info(`Toggled autoUpdate to ${userPreferences.autoUpdate}`);
+      },
+      type: 'checkbox',
+      checked: userPreferences.autoUpdate,
+  }));
+
+  settingsMenu.append(new nw.MenuItem({
+      label: 'Automatically check for firmware updates',
+      click: function() {
+          userPreferences.autoUpdateFW = !userPreferences.autoUpdateFW;
+          console.info(`Toggled autoUpdateFW to ${userPreferences.autoUpdateFW}`);
+      },
+      type: 'checkbox',
+      checked: userPreferences.autoUpdateFW,
+  }));
+
+  const tray = new nw.Tray({
+    icon: './images/ok-tray-logo.png',
+    menu: settingsMenu,
+    tooltip: 'OnlyKey Configuration App settings'
+  });
+})();
