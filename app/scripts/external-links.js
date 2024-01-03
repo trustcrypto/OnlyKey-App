81b9472b85c91ef052c78eefd56e29ead79d3d93
Rodney G <rodney@golpe.us>
2024-01-02 23:21:01 -0500

    fix certain external-links open in app window

--- app/scripts/external-links.js
+++ app/scripts/external-links.js
@@ -1,9 +1,12 @@
-const handler = evt => {
-    const parent = (evt.path && evt.path[1]) || {};
-    const href = evt.target && evt.target.href ? evt.target.href : parent.href;
+const openMethod = typeof nw === 'undefined' ? window.open : nw.Shell.openExternal;
 
-    if ((evt.target !== evt.currentTarget) && (!!href && (evt.target.classList.contains('external') || parent.classList.contains('external')))) {
-        const openMethod = typeof nw === 'undefined' ? window.open : nw.Shell.openExternal;
+const handler = evt => {
+    let href = evt.target && evt.target.href;
+    if (!href) href = evt.target && evt.target.offsetParent && evt.target.offsetParent.href;
+    if (!href) href = evt.path && evt.path[1] && evt.path[1].href;
+    
+    // Formerly checked for 'external' in classList. Now fires for all https:// links.
+    if (!!href && href.indexOf('https://') == 0) {
         openMethod(href);
         evt.preventDefault && evt.preventDefault();
         evt.stopPropgation && evt.stopPropagation();
